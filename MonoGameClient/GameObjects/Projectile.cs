2dc28609a15a50a0986476577eb3adacd181ecd0
S00164384 <s00164384@mail.itsligo.ie>
2018-01-11 23:08:07 +0000

    noo

---
+++ MonoGameClient/GameObjects/Projectile.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Turrets
+{
+    public class Projectile : DrawableGameComponent
+    {
+        private float _timer;
+        private float Alive = 3f;
+        private float angle;
+        bool visible = true;
+        private int Speed = 4;
+        private Vector2 Direction;
+        private Vector2 Position;
+
+        public Projectile (Texture2D texture,float t,Game g,float a,Vector2 pos) : base(g)
+        {
+            _timer = 0f;
+            Alive = t;
+            angle = a;
+
+        }
+
+        public override void Update(GameTime gametime)
+        {
+            if (!visible) return;
+            _timer += (float)gametime.ElapsedGameTime.TotalSeconds;
+
+            if (_timer > Alive)
+                visible = false;
+
+            Position += Direction * Speed;
+            base.Update(gametime);
+        }
+    }
+}
---
+++ MonoGameClient/GameObjects/Turret.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Turrets
+{
+    public class Turret : DrawableGameComponent
+    {
+        public Projectile Projectile;
+        Texture2D _tx;
+        public Vector2 position;
+        public float angle = 0f;
+        Vector2 origin;
+
+        //======================================================================================================
+
+        public Turret(Texture2D texture, Game g,Vector2 pos) : base(g)
+        {
+            g.Components.Add(this);
+            _tx = texture;
+            position = pos;
+            origin = new Vector2(_tx.Width/2 + position.X,_tx.Height / 2+position.Y);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            origin = new Vector2(_tx.Width / 2 + position.X/2, _tx.Height / 2 + position.Y/2);
+            base.Update(gameTime);
+        }
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch sb = Game.Services.GetService<SpriteBatch>();
+            if (sb == null) return;
+            sb.Begin();
+            sb.Draw(_tx, position, null, Color.White, angle, origin, 1f, SpriteEffects.None, 0);
+            sb.End();
+            base.Draw(gameTime);
+        }
+        //======================================================================================================
+
+        //public override void Update(GameTime gametime, List<Sprite> sprites)
+        //{
+        //    _previousKey = _currentKey;
+        //    _currentKey = Keyboard.GetState();
+
+
+        //    if (_currentKey.IsKeyDown(Keys.A)) _rotation -= MathHelper.ToRadians(RotationSpeed);
+        //    else if (_currentKey.IsKeyDown(Keys.D)) _rotation += MathHelper.ToRadians(RotationSpeed);
+
+        //    Direction = new Vector2((float)Math.Cos(_rotation), (float)Math.Sin(_rotation));
+
+        //    if (_currentKey.IsKeyDown(Keys.W))
+        //        Position += Direction * Speed;
+
+        //    if (_currentKey.IsKeyDown(Keys.Space) &&
+        //            _previousKey.IsKeyUp(Keys.Space))
+        //    {
+        //        AddProjectile(sprites);
+        //    }
+
+        //}
+
+        ////======================================================================================================
+
+        //private void AddProjectile(List<Sprite>sprites)
+        //    { 
+        //                var projectile = Projectile.Clone() as Projectile;
+        //                projectile.Direction = this.Direction;
+        //                projectile.Position = this.Position;
+        //                projectile.Speed = this.Speed * 2;
+        //                projectile.Alive = 2f;
+
+        //                sprites.Add(projectile);
+        //     }
+
+        //    }
+    }
+}
+
+
